88691fab162fe9563fb754e3d84c8cec82db1171
chritianegozza <christiane_natacha@hotmail.com>
2022-05-29 18:00:30 -0300

    first commit

---
+++ src/br/com/dio/exercicios/arrays/loops/Ex1_NomeEIdade.java
@@ -0,0 +1,23 @@
+package br.com.dio.exercicios.arrays.loops;
+
+import java.util.Scanner;
+
+public class Ex1_NomeEIdade {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String nome;
+        int idade;
+        while (true) {
+
+            System.out.println("nome: ");
+            nome = scan.next();
+            if (nome.equals("0") ) break;
+
+            System.out.println("Idade: ");
+            idade = scan.nextInt();
+        }
+        System.out.println("Continua aqui ...");
+
+    }
+}
